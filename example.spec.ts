2d4c0eb855d0c46accf82acceb3450ba3e315d77
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2026-05-18 21:53:34 -0300

    feat: test home and form screen

---
+++ example.spec.ts
@@ -0,0 +1,75 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Página Home', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000');
+  });
+
+  test('deve exibir os metadados e elementos principais corretamente', async ({ page }) => {
+    await expect(page).toHaveTitle('React App');
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Home',
+      })
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole('link', { name: 'Form' })
+    ).toBeVisible();
+  });
+
+  test('deve redirecionar para a página de formulário ao clicar no link', async ({
+    page,
+  }) => {
+    await page.getByRole('link', { name: 'Form' }).click();
+
+    await expect(page).toHaveTitle('Form');
+  });
+});
+
+test.describe('Página Formulário', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:3000/form');
+  });
+
+  test('deve exibir os metadados e componentes do formulário corretamente', async ({
+    page,
+  }) => {
+    await expect(page).toHaveTitle('Form');
+
+    await expect(
+      page.getByRole('heading', {
+        name: 'Form',
+      })
+    ).toBeVisible();
+
+    await expect(
+      page.getByPlaceholder('Enter item')
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole('button', { name: 'Add' })
+    ).toBeVisible();
+  });
+
+  test('deve iniciar com a lista de itens vazia', async ({ page }) => {
+    const itemsList = page.getByTestId('items-list');
+
+    await expect(itemsList).toBeEmpty();
+  });
+
+  test('deve adicionar um novo item à lista', async ({ page }) => {
+    const input = page.getByPlaceholder('Enter item');
+
+    await input.fill('Sabrina');
+
+    await page.getByRole('button', { name: 'Add' }).click();
+
+    const item = page.getByTestId('item').nth(0);
+
+    await expect(item).toHaveText('Sabrina');
+
+    await expect(input).toBeEmpty();
+  });
+});
